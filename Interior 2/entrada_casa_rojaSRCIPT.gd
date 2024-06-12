0fd1063fea7c3846d62b51f7282509342e902160
felix11111 <11felixfenix11@gmail.com>
2024-06-12 17:27:03 -0300

    corregido

--- Interior 2/entrada_casa_rojaSRCIPT.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func _on_body_entered(body):
-	if body.name == "CharacterBody2D":
-		get_tree().change_scene_to_file("res://CASA ROJA.tscn")
-		body.name = ""
-	if body.name != "CharacterBody2D":
-		get_tree().change_scene_to_file("res://JUEGO/Beta.tscn")
-
