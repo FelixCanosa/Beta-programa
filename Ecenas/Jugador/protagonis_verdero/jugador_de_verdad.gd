ee61e6d76753f2ad87b32593bac5a32614426ff9
felix11111 <11felixfenix11@gmail.com>
2024-08-25 13:54:43 -0300

    sea actualizo el motor a 4.3

--- Ecenas/Jugador/protagonis_verdero/jugador_de_verdad.gd
+++ Ecenas/Jugador/protagonis_verdero/jugador_de_verdad.gd
@@ -13,13 +13,13 @@ func _ready():
 #	$Modelo.visible = false
 
 	
-func _process(delta):
+func _process(_delta):
 	# Comprueba si el jugador ha presionado la tecla "R"
 	if Input.is_action_just_pressed("AprecerAcistente"):
 		# Cambia la visibilidad de la interfaz
 		$Modelo.visible = !$Modelo.visible
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var move_direciton = Vector2(
 		Input.get_action_strength("drecha") - Input.get_action_strength("izquierda"),
 		Input.get_action_strength("abajo") - Input.get_action_strength("arriba"),
@@ -37,5 +37,3 @@ func update_animations(move_direciton):
 	else:
 		animationa_playback.travel("Reposo") 
 		
-
-
