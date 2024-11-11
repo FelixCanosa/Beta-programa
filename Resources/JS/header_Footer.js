f53ea926a46eec6608bd4e5e66e2f35a0bbd9daf
FelixCanosa <11felixfenix11@gmail.com>
2024-11-10 21:02:07 -0300

    agregando ai.1.5

--- Resources/JS/header_Footer.js
+++ Resources/JS/header_Footer.js
@@ -12,7 +12,7 @@ function createHeaderAndFooter() {
 const headerContent = `
     <nav class="navbar navbar-expand-lg bg-light shadow-sm mb-4">
         <div class="container-fluid">
-            <a class="navbar-brand text-primary" href="#">··</a>
+            <a class="navbar-brand text-primary" href="#"><i class="bi bi-dice-5"></i></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
